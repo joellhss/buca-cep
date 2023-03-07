3e051455a895a86d9a5378f5539df737ac038db9
Joel Henrique <joelhenrique78@gmail.com>
2023-03-07 10:36:17 -0300

    Busca incial no ViaCep

--- example.php
+++ example.php
@@ -1,1 +1,11 @@
 <?php
+
+require_once "vendor/autoload.php";
+
+use \Joel\BuscaCep\Search;
+
+$busca = new Search();
+
+$resultado = $busca -> getAddressFromZipcode('56332-016');
+
+var_dump($resultado);
